f4cd3e09e644fc71bc16895fecccc5885ee9b99f
Patrick Plenefisch <simonpatp@gmail.com>
2024-11-11 13:18:39 -0500

    lvm: Add support for cachevol lv
    Mark cachevol lv's as ignored features, which is true
    only if they are configured as "writethrough". This patch
    does not let grub boot from "writeback" cache-enabled lv's
    
    Signed-off-by: Patrick Plenefisch <simonpatp@gmail.com>
    Reviewed-by: Daniel Kiper <daniel.kiper@oracle.com>

--- grub-core/disk/lvm.c
+++ grub-core/disk/lvm.c
@@ -823,6 +823,8 @@ grub_lvm_detect (grub_disk_t disk,
 		   */
 		  else if (grub_strncmp (p, "cache\"",
 				   sizeof ("cache\"") - 1) == 0
+				   || grub_strncmp (p, "cache+CACHE_USES_CACHEVOL\"",
+				   sizeof ("cache+CACHE_USES_CACHEVOL\"") - 1) == 0
 				   || grub_strncmp (p, "integrity\"",
 				   sizeof ("integrity\"") - 1) == 0)
 		    {
